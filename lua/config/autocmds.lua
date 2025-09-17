6e9de6c123c4ef7725738975c20825197600334b
Jingkai He <jingkai@hey.com>
2025-09-17 17:17:10 +0000

    chore: update LazyVim configuration and add markdown autocmd
    Co-authored-by: Kodelet <noreply@kodelet.com>
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable concealing for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "md" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
