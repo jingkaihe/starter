b6eaf3231da3ff3069c522fe41a06c6b3b209a5b
Jingkai He <jingkai@hey.com>
2025-09-18 21:20:21 +0000

    chore: update lazy-lock.json and lazyvim.json with latest plugin versions and add snacks.nvim configuration
    Co-authored-by: Kodelet <noreply@kodelet.com>
    Signed-off-by: Jingkai He <jingkai@hey.com>

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true, -- show hidden files by default
+        },
+        explorer = {
+          hidden = true, -- show hidden files in explorer by default
+        },
+      },
+    },
+  },
+}
