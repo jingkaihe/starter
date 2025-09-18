146a14a260a729ea200b0da010ebc1971381c1ec
Jingkai <jingkai@hey.com>
2025-09-18 16:59:54 +0100

    feat(go): add Go language support with gopls inlay hints disabled
    Co-authored-by: Kodelet <noreply@kodelet.com>
    Signed-off-by: Jingkai <jingkai@hey.com>

---
+++ lua/plugins/go.lua
@@ -0,0 +1,28 @@
+return {
+  -- Import LazyVim Go language support
+  { import = "lazyvim.plugins.extras.lang.go" },
+
+  -- Configure gopls to disable inlay hints
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
