094efcc7622e209c3b87b9640aa85b2d8b46940c
Jingkai He <jingkai@hey.com>
2025-09-23 13:46:31 +0000

    chore: update LazyVim configuration and enable gitignored file display
    Co-authored-by: Kodelet <noreply@kodelet.com>
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,10 +4,11 @@ return {
     picker = {
       sources = {
         files = {
-          hidden = true,  -- show hidden files by default
+          hidden = true, -- show hidden files by default
+          ignored = true, -- show gitignored files by default
         },
         explorer = {
-          hidden = true,  -- show hidden files in explorer by default
+          hidden = true, -- show hidden files in explorer by default
           ignored = true, -- show gitignored files in explorer by default
         },
       },
